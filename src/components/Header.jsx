00d9142a8d795dc2aae37b57c5c7d962e3642165
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-09 03:42:02 -0600

    use cart context in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 const Header = () => {
+  const { items } = useContext(CartContext);
+
+  const cartQuantity = items.length
+  const totalCartItems = items.reduce((totalNumbersOfItems, item) => {
+    return totalNumbersOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +17,7 @@ const Header = () => {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button>Cart (0)</Button>
+        <Button textOnly>Cart ({cartQuantity})</Button>
       </nav>
     </header>
   );
