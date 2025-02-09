9d1bdf4d7e64c2b62bf86cdf1600f4447616076b
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-09 14:43:36 -0600

    use create portal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 import { createPortal } from "react-dom";
 
-const Modal = () => {
-  return createPortal(<dialog></dialog>, document.getElementById("modal"));
+const Modal = ({ children,open }) => {
+  return createPortal(
+    <dialog>{children}</dialog>,
+    document.getElementById("modal")
+  );
 };
 
 export default Modal;
