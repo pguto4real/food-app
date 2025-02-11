f5389806796121c7f44a77ea55ef9edf5e33cbee
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-11 09:13:52 -0600

    update use fetch and use it in meals.jsx

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import { useHttp } from "../hooks/useHttp";
 
 const Meals = () => {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  //   const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    fetchedData: loadedMeals,
+    isFetching,
+    error,
+  } = useHttp("http://localhost:3000/meals",{},[]);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  //   useEffect(() => {
+  //     async function fetchMeals() {
+  //       const response = await fetch("http://localhost:3000/meals");
 
-      if (!response.ok) {
-        return;
-      }
+  //       if (!response.ok) {
+  //         return;
+  //       }
 
-      const mealsData = await response.json();
-      setLoadedMeals(mealsData);
-    }
-    fetchMeals();
-  }, []);
+  //       const mealsData = await response.json();
+  //       setLoadedMeals(mealsData);
+  //     }
+  //     fetchMeals();
+  //   }, []);
+//   console.log(loadedMeals);
+
+if(isFetching){
+    return <p>Fetching meals...</p>
+}
   return (
     <ul id="meals">
       {loadedMeals.map((loadedMeal, index) => (
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error(
+      resData.message || "Something went wrong, Failed to perform request"
+    );
+  }
+
+  return resData;
+}
+
+export function useHttp(url, config, initialValue) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  const sendRequest = useCallback(
+    async function sendRequest() {
+      setIsFetching(true);
+      try {
+        const resData = await sendHttpRequest(url, config);
+        setFetchedData(resData);
+      } catch (error) {
+        setError(error.message || "Failed to fetch user places.");
+      }
+      setIsFetching(false);
+    },
+    [url, config]
+  );
+
+  useEffect(() => {
+    if (config && (config.method === "GET" || !config.method)) {
+      sendRequest();
+    }
+  }, []);
+
+  return {
+    isFetching,
+    fetchedData,
+    error,
+    setFetchedData,
+  };
+}
