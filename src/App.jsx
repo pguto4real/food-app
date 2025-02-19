5775b0ef77bc6cfb9218f31b56c7d4994684969c
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-18 20:36:54 -0600

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ function App() {
         <Meals />
         <Cart />
         <Checkout />
-        {/* <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p> */}
+        
       </CartContextProvider>
     </UserProgressContextProvider>
   );
